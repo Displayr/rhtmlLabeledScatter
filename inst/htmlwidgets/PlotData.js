85b6526c8a6b9278f478b697a3d5d2bdd8db2bec
Po Liu <pliu0771@uni.sydney.edu.au>
2016-06-21 09:33:28 +1000

    default qColors

--- inst/htmlwidgets/PlotData.js
+++ inst/htmlwidgets/PlotData.js
@@ -2,12 +2,14 @@
 var PlotData;
 
 PlotData = (function() {
-  function PlotData(X, Y, group, label, viewBoxDim) {
+  function PlotData(X, Y, group, label, viewBoxDim, colors) {
     this.X = X;
     this.Y = Y;
     this.group = group;
     this.label = label;
     this.viewBoxDim = viewBoxDim;
+    this.colorWheel = colors ? colors : ['#5B9BD5', '#ED7D31', '#A5A5A5', '#1EC000', '#4472C4', '#70AD47', '#255E91', '#9E480E', '#636363', '#997300', '#264478', '#43682B', '#FFFFFF', '#FF2323'];
+    this.cIndex = 0;
     if (this.X.length === this.Y.length) {
       this.len = X.length;
       this.normalizeData();
@@ -51,12 +53,11 @@ PlotData = (function() {
   };
 
   PlotData.prototype.initDataArrays = function() {
-    var color, group, i, newColor, _results;
+    var group, i, newColor, _results;
     this.pts = [];
     this.lab = [];
     this.anc = [];
     this.legend = [];
-    color = new RColor;
     group = this.group;
     i = 0;
     _results = [];
@@ -64,7 +65,7 @@ PlotData = (function() {
       if (!(_.some(this.legend, function(e) {
         return e.text === group[i];
       }))) {
-        newColor = color.get(true, 0.9, 0.9);
+        newColor = this.getDefaultColor();
         this.legend.push({
           text: this.group[i],
           color: newColor
@@ -93,6 +94,10 @@ PlotData = (function() {
       _results.push(i++);
     }
     return _results;
+  };
+
+  PlotData.prototype.getDefaultColor = function() {
+    return this.colorWheel[(this.cIndex++) % this.colorWheel.length];
   };
 
   return PlotData;
--- theSrc/scripts/PlotData.coffee
+++ theSrc/scripts/PlotData.coffee
@@ -1,10 +1,29 @@
 class PlotData
-  constructor: (X, Y, group, label, viewBoxDim) ->
+  constructor: (X, Y, group, label, viewBoxDim, colors) ->
     @X = X
     @Y = Y
     @group = group
     @label = label
     @viewBoxDim = viewBoxDim
+
+    @colorWheel = if colors then colors else [ # default qColors
+      '#5B9BD5'
+      '#ED7D31'
+      '#A5A5A5'
+      '#1EC000'
+      '#4472C4'
+      '#70AD47'
+      '#255E91'
+      '#9E480E'
+      '#636363'
+      '#997300'
+      '#264478'
+      '#43682B'
+      '#FFFFFF'
+      '#FF2323'
+    ]
+    @cIndex = 0
+
     if @X.length is @Y.length
       @len = X.length
       @normalizeData()
@@ -37,13 +56,15 @@ class PlotData
     @lab = []
     @anc = []
     @legend = []
-    color = new RColor #using rColor library to gen random colours
     group = @group
+
+    # color = new RColor #using rColor library to gen random colours
 
     i = 0
     while i < @len
       unless (_.some @legend, (e) -> e.text is group[i])
-        newColor = color.get(true, 0.9, 0.9)
+        # newColor = color.get(true, 0.9, 0.9)
+        newColor = @getDefaultColor()
         @legend.push {text: @group[i], color: newColor}
       @pts.push({
         x: @X[i]*@viewBoxDim.width + @viewBoxDim.x
@@ -66,3 +87,6 @@ class PlotData
         r: 2
       })
       i++
+
+  getDefaultColor: ->
+    @colorWheel[(@cIndex++)%(@colorWheel.length)]
