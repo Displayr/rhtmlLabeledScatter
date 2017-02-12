1ab67d0feaef003958d2f5e24853237400bbdc8e
Po Liu <po.liu.2s@gmail.com>
2017-02-12 13:07:45 +1100

    Split out gulp tasks

---
+++ build/tasks/makeDocs.js
@@ -0,0 +1,7 @@
+const gulp = require('gulp');
+const shell = require('gulp-shell');
+
+gulp.task('makeDocs', function () {
+    return gulp.src('../makeDoc.r', {read: false})
+        .pipe(shell(['R --no-save < <%= file.path %>', ], {}))
+});
