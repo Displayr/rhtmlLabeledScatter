493aa2729fce05888c1e0e8ff2a4a171c185c414
Kyle Zeeuwen <kyle.zeeuwen@gmail.com>
2017-05-08 17:27:30 +1000

    do not watch test files

--- build/tasks/watch.js
+++ build/tasks/watch.js
@@ -4,15 +4,15 @@ const gulp = require('gulp');
 gulp.task('watch', ['connect'], function () {
   $.livereload.listen();
 
-    // watch for changes in the browser directory and reload chrome on changes
+  // watch for changes in the browser directory and reload chrome on changes
   gulp.watch([
     'browser/**/*',
   ]).on('change', $.livereload.changed);
 
-    // when these files change then do this,
-    // for example when the json file changes rerun the copy command
+  // when these files change then do this,
+  // for example when the json file changes rerun the copy command
   gulp.watch('theSrc/internal_www/**/*', ['copy']);
   gulp.watch('theSrc/styles/**/*.less', ['less']);
-  gulp.watch('theSrc/scripts/**/*.js', ['compileES6ToInst', 'compileRenderContentPage']);
+  gulp.watch(['theSrc/scripts/**/*.js', '!theSrc/scripts/**/*.spec.js'], ['compileES6ToInst', 'compileRenderContentPage']);
   gulp.watch('theSrc/scripts/data/**/*', ['copy']);
 });
