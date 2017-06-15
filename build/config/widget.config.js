6d24a24d39d5f0445ac855fcff822a8bddb780d8
Kyle Zeeuwen <kyle.zeeuwen@gmail.com>
2017-06-16 07:31:04 +1000

    use rhtmlBuildUtils v1.1.1

--- build/config/widget.config.js
+++ build/config/widget.config.js
@@ -1,5 +1,4 @@
 module.exports = {
   widgetEntryPoint: 'theSrc/scripts/rhtmlLabeledScatter.js',
-  widgetName: 'rhtmlLabeledScatter',
-  rFunction: 'rhtmlLabeledScatter::LabeledScatter'
+  widgetName: 'rhtmlLabeledScatter'
 }
