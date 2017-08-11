de24db158ae39f7da33dbc931a7b91ab9529e373
Po Liu <po.liu.2s@gmail.com>
2017-08-11 18:12:57 +1000

    VIS-321: Make grid origin lines more visible

--- theSrc/scripts/utils/AxisUtils.js
+++ theSrc/scripts/utils/AxisUtils.js
@@ -110,7 +110,8 @@ class AxisUtils {
           x1,
           y1: y2,
           x2: x1,
-          y2: y2 + leaderLineLen
+          y2: y2 + leaderLineLen,
+          num: label
         })
         axisLeaderLabelStack.push({
           x: x1,
