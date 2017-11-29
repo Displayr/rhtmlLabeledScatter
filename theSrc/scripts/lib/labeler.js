418c1f6f2717c26d0fc00cf83e755f578f62ca74
Po Liu <po.liu.2s@gmail.com>
2017-11-29 15:14:07 +1100

    VIS-369: Reduce penalty to half

--- theSrc/scripts/lib/labeler.js
+++ theSrc/scripts/lib/labeler.js
@@ -148,6 +148,9 @@ const labeler = function () {
       x_overlap = Math.max(0, Math.min(x12, x22) - Math.max(x11, x21))
       y_overlap = Math.max(0, Math.min(y12, y22) - Math.max(y11, y21))
       overlap_area = x_overlap * y_overlap
+      if (isBubble && a.id === index) {
+        overlap_area /= 2
+      }
       ener += (overlap_area * w_lab_anc)
     })
     return ener
