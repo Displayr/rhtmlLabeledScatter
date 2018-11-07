db24d959b7e0b3239281e65ef748a27c4d45fa4a
Po Liu <po.liu.2s@gmail.com>
2018-11-07 22:38:52 +1030

    further simplify css

--- theSrc/scripts/RectPlot.js
+++ theSrc/scripts/RectPlot.js
@@ -612,7 +612,7 @@ class RectPlot {
           content: function () { // the html content to show inside the tooltip
             return `<span style='font-size: 11px; text-align: center;'>${d.label}</span>`
           },
-          template: '<div class="popover" role="tooltip"><div class="arrow" style="left: -3px; bottom: -55%; border-top-color: rgba(0,0,0,.25);"></div><div class="popover-body" style="padding: 9px 14px;"></div></div>'
+          template: '<div class="popover" style="pointer-events: none;" role="tooltip"><div class="arrow" style="left: -3px; bottom: -55%; border-top-color: rgba(0,0,0,.25);"></div><div class="popover-body" style="padding: 9px 14px;"></div></div>'
         }
         )
         // $(element).popover('show') - this doens't work
