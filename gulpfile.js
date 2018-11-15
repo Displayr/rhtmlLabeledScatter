ec458e0f04b1f111d68069ad23020f31460be728
Po Liu <po.liu.2s@gmail.com>
2018-11-15 16:53:56 +1030

    bootstrap-copy task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 const gulp = require('gulp')
 const rhtmlBuildUtils = require('rhtmlBuildUtils')
 
-const dontRegisterTheseTasks = []
+const dontRegisterTheseTasks = ['core']
 rhtmlBuildUtils.registerGulpTasks({ gulp, exclusions: dontRegisterTheseTasks })
+
+gulp.task('bootstrap-copy', function () {
+  const internalWebServerDependencies = [
+    'node_modules/bootstrap/dist/js/bootstrap.min.js'
+  ]
+
+  gulp.src(internalWebServerDependencies)
+      .pipe(gulp.dest('browser/internal_www/external/'))
+})
+gulp.task('core', ['less', 'copy', 'bootstrap-copy'])
