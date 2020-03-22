c8ae93bdd75ee69185a95d195d0a0ad8d083c1a4
Kyle Zeeuwen <kyle.zeeuwen@gmail.com>
2020-03-22 14:35:48 +1000

    labeler: test: add chooseBestLeaderLine spec test

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 const path = require('path')
 const gulp = require('gulp')
+const shell = require('shelljs')
 const rhtmlBuildUtils = require('rhtmlBuildUtils')
 
 const dontRegisterTheseTasks = ['testVisual', 'testVisual_s', 'serve']
 rhtmlBuildUtils.registerGulpTasks({ gulp, exclusions: dontRegisterTheseTasks })
 
-const shell = require('shelljs')
 const jestSnapshotTests = () => {
   return function (done) {
     const testPath = path.join(__dirname, '/theSrc/test/')
@@ -15,7 +15,33 @@ const jestSnapshotTests = () => {
      * new snapshots are not written in --ci mode
      * we have different snapshot dirs for local vs ci as the snapshots are slightly different in travis` linux ci.
      */
-    return shell.exec(`jest ${testPath} --ci=0`, { async: true }, (exitCode) => {
+    return shell.exec(`jest --rootDir=${testPath} --testMatch='**/*.jest.test.js' --ci=0`, { async: true }, (exitCode) => {
+      const error = (exitCode === 0) ? null : new Error(`jest ${testPath} failed with code ${exitCode}`)
+      done(error)
+
+      /* connect is leaving the server running, so gulp will not exit. This is a hacky way of getting gulp to exit and maintaining the test exit code
+       * Main issue I can see with this approach is that now jestSnapshotTests MUST be the last task to run
+       * Note that runProtractor.js (in rhtmlBuildUtils) handled this by properly using gulp streams and then piping to gulpExit
+       */
+      setTimeout(() => {
+        process.exit(exitCode)
+      }, 200)
+    })
+  }
+}
+
+// default testMatch : **/__tests__/**/*.[jt]s?(x), **/?(*.)+(spec|test).[tj]s?(x)
+
+const jestSpecTests = () => {
+  return function (done) {
+    const testPath = path.join(__dirname, '/theSrc/scripts/')
+
+    /* --ci=0 allows new snapshots to be written while in travisci
+     * jest auto detects travis ci and changes behaviour
+     * new snapshots are not written in --ci mode
+     * we have different snapshot dirs for local vs ci as the snapshots are slightly different in travis` linux ci.
+     */
+    return shell.exec(`jest --rootDir=${testPath} --testMatch='**/*.jest.test.js' --ci=0`, { async: true }, (exitCode) => {
       const error = (exitCode === 0) ? null : new Error(`jest ${testPath} failed with code ${exitCode}`)
       done(error)
 
@@ -64,6 +90,7 @@ gulp.task('compileCrossExperimentSnapshotComparison', require('./build/tasks/com
 gulp.task('compileCrossExperimentSnapshotList', require('./build/tasks/compileCrossExperimentSnapshotList')(gulp))
 gulp.task('copyExperimentSnapshots', copyExperimentSnapshots(gulp))
 gulp.task('jestSnapshotTests', jestSnapshotTests(gulp))
+gulp.task('jestSpecTests', jestSpecTests(gulp))
 gulp.task('testVisual', gulp.series('core', 'compileInternal', 'connect', 'jestSnapshotTests'))
 gulp.task('testVisual_s', gulp.series('jestSnapshotTests'))
 
---
+++ theSrc/scripts/lib/labeler.jest.test.js
@@ -0,0 +1,44 @@
+import labeler from './labeler'
+
+// x/y from center
+const getLabel = ({ w = 2, h = 2, x = 10, y = 10 } = {}) => ({
+  width: w,
+  height: h,
+  minX: x - w / 2,
+  maxX: x + w / 2,
+  minY: y - h / 2,
+  maxY: y + h / 2
+})
+
+const getAnchor = ({ r = 2, x = 10, y = 10 } = {}) => ({
+  r,
+  x,
+  y,
+  minX: x - r,
+  maxX: x + r,
+  minY: y - r,
+  maxY: y + r
+})
+
+const basicBehaviourTestConfig = {
+  anchor: getAnchor({ r: 2, x: 10, y: 10 }),
+  labelPositionsAndExpectations: [
+    ['centerBottomDistance', getLabel({ x: 10, y: 5 })],
+    ['centerTopDistance', getLabel({ x: 10, y: 15 })],
+    ['leftCenterDistance', getLabel({ x: 15, y: 10 })],
+    ['rightCenterDistance', getLabel({ x: 5, y: 10 })],
+    ['leftTopDistance', getLabel({ x: 15, y: 15 })],
+    ['rightBottomDistance', getLabel({ x: 5, y: 5 })],
+    ['rightTopDistance', getLabel({ x: 5, y: 15 })],
+    ['leftBottomDistance', getLabel({ x: 15, y: 5 })]
+  ]
+}
+
+describe('chooseBestLeaderLine:', () => {
+  test.each(basicBehaviourTestConfig.labelPositionsAndExpectations)(`%#: %s`, (chosenPosition, label) => {
+    const option = labeler()
+      .chooseBestLeaderLine(label, basicBehaviourTestConfig.anchor)
+
+    expect(option.name).toBe(chosenPosition)
+  })
+})
