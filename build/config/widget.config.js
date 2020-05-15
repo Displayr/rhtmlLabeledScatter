e707a8b27133a587a4b1c2c4e94d949407358d18
Kyle Zeeuwen <kyle.zeeuwen@gmail.com>
2020-05-15 17:32:36 +1000

    use rhtmlBuildUtils 5.0.0

--- build/config/widget.config.js
+++ build/config/widget.config.js
@@ -2,55 +2,36 @@ const cliArgs = require('yargs').argv
 const _ = require('lodash')
 
 const config = {
-  'widgetEntryPoint': 'theSrc/scripts/rhtmlLabeledScatter.js',
-  'widgetFactory': 'theSrc/scripts/rhtmlLabeledScatter.factory.js',
-  'widgetName': 'rhtmlLabeledScatter',
-  'internalWebSettings': {
-    'isReadySelector': '.rhtmlLabeledScatter-isReadySelector',
-    'singleWidgetSnapshotSelector': 'svg.rhtmlwidget-outer-svg',
-    'default_border': true,
-    'css': [
+  widgetEntryPoint: 'theSrc/scripts/rhtmlLabeledScatter.js',
+  widgetFactory: 'theSrc/scripts/rhtmlLabeledScatter.factory.js',
+  widgetName: 'rhtmlLabeledScatter',
+  internalWebSettings: {
+    isReadySelector: '.rhtmlLabeledScatter-isReadySelector',
+    singleWidgetSnapshotSelector: 'svg.rhtmlwidget-outer-svg',
+    default_border: true,
+    css: [
       '/styles/rhtmlLabeledScatter.css'
     ]
   },
-  'snapshotTesting': {
-    'testEnv': 'local', // local || travis
-    'branch': 'master', // git branch (you dont need to change it; is overridden auto when npm run travisTest is run)
-    'testplanDirectory': 'theSrc/test/snapshotTestDefinitions',
-    'snapshotDirectory': 'theSrc/test/snapshots',
-    'timeout': 30000,
-    'snapshotDelay': 100,
-    'puppeteer': {
-      'headless': 1, // 0 | 1 | true | false
-      'slowMo': 0,
-      'defaultViewport': {
-        'width': 1600,
-        'height': 1600
+  snapshotTesting: {
+    consoleLogHandler: (msg, testName) => {
+      const statsLineString = _(msg.args())
+        .map(arg => `${arg}`)
+        .filter(arg => arg.match(/duration/))
+        .first()
+
+      if (statsLineString) {
+        const statsStringMatch = statsLineString.match('^JSHandle:(.+)$')
+        if (statsStringMatch) {
+          const stats = JSON.parse(statsStringMatch[1])
+          console.log(JSON.stringify(_.assign(stats, { scenario: testName })))
+        }
       }
-    },
-    'pixelmatch': {
-      // smaller values -> more sensitive : https://github.com/mapbox/pixelmatch#pixelmatchimg1-img2-output-width-height-options
-      'customDiffConfig': {
-        'threshold': 0.1
-      },
-      'failureThreshold': 0.01,
-      'failureThresholdType': 'percent' // pixel or percent
     }
   }
 }
 
 const commandLineOverides = _.omit(cliArgs, ['_', '$0'])
 const mergedConfig = _.merge(config, commandLineOverides)
-
-// this is a hack to allow configuring just the snapshotTesting.testEnv and snapshotTesting.branch
-// the issue is that tests are invoked by jest as child processes and the command line args do not propogate
-
-// node gulp testVisual --snapshotTesting.testEnv=travis --snapshotTesting.branch=VIS-513
-  // the gulp task explicitly echoes the command line params
-  // jest <project_root>/theSrc/test/ --snapshotTesting.testEnv=travis --snapshotTesting.branch=VIS-513
-    // node /Users/kyle/projects/numbers/scatter/node_modules/jest-worker/build/workers/processChild.js
-
-if (process.env.ENV) { mergedConfig.snapshotTesting.testEnv = process.env.ENV }
-if (process.env.BRANCH) { mergedConfig.snapshotTesting.branch = process.env.BRANCH }
 
 module.exports = mergedConfig
