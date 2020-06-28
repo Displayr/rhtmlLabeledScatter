57d0a93315a5d775e31a61226aa9a434c9f0cf6a
Kyle Zeeuwen <kyle.zeeuwen@gmail.com>
2020-06-28 18:46:32 +1000

    new test: resize: legend labels preserved, plot moves are reset

---
+++ theSrc/test/bin/resize.jest.test.js
@@ -0,0 +1,78 @@
+const puppeteer = require('puppeteer')
+const { snapshotTesting: { renderExamplePageTestHelper } } = require('rhtmlBuildUtils')
+const loadWidget = require('../lib/loadWidget.helper')
+
+const {
+  configureImageSnapshotMatcher,
+  puppeteerSettings,
+  testSnapshots,
+  jestTimeout
+} = renderExamplePageTestHelper
+
+jest.setTimeout(jestTimeout)
+configureImageSnapshotMatcher('resize')
+
+describe('resize', () => {
+  let browser
+
+  beforeEach(async () => {
+    browser = await puppeteer.launch(puppeteerSettings)
+  })
+
+  afterEach(async () => {
+    await browser.close()
+  })
+
+  test('basic resize', async function () {
+    const { page } = await loadWidget({
+      browser,
+      configName: 'data.bdd.legend_drag_test_plot',
+      width: 500,
+      height: 500
+    })
+
+    await testSnapshots({ page, snapshotName: 'basic_initial' })
+
+    await page.evaluate(() => {
+      window.resizeHook(750, 750)
+    })
+
+    await page.waitFor(1000)
+
+    await testSnapshots({ page, snapshotName: 'basic_after_resize' })
+
+    await page.close()
+  })
+
+  test('user interactions on resize: on plot are reset, on legend are maintained', async function () {
+    const { page, scatterPlot } = await loadWidget({
+      browser,
+      configName: 'data.bdd.three_point_brand',
+      width: 500,
+      height: 500
+    })
+
+    await testSnapshots({ page, snapshotName: 'user_interaction_initial' })
+
+    await scatterPlot.movePlotLabel({ id: 0, x: 50, y: 50 })
+    await scatterPlot.movePlotLabelToLegend({ id: 1 })
+
+    await testSnapshots({ page, snapshotName: 'user_interaction_after_interaction' })
+
+    await page.evaluate(() => {
+      window.resizeHook(750, 750)
+    })
+
+    await page.waitFor(1000)
+
+    await testSnapshots({ page, snapshotName: 'user_interaction_after_resize' })
+
+    await page.close()
+  })
+})
+
+
+// DONT USER page.waitForFunction !
+// return page.evaluate(getStateUpdates).then((stateUpdates) => {
+//   return stateUpdates[stateUpdates.length - 1]
+// })
