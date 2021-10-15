e77df50c65209b20882ab2e8f983b3b119d637f0
Justin Yap <JustinCCYap@users.noreply.github.com>
2021-10-15 17:27:41 +1100

    Fix test

--- theSrc/scripts/utils/LegendUtils.jest.test.js
+++ theSrc/scripts/utils/LegendUtils.jest.test.js
@@ -2,23 +2,25 @@ const LegendUtils = require('./LegendUtils.js')
 
 describe('LegendUtils:', function () {
   describe('getLegendBubbles():', function () {
-    it('computes top, middle, and bottom legend bubble sizes for zMax=10000', function () {
+    it('computes large, medium and small legend bubble sizes for zMax=10000', function () {
       const legendBubbles = LegendUtils.getLegendBubbles(10000, '$', '')
 
       expect(legendBubbles).toEqual({
-        large: { label: '$9.00k', size: 0.5352372348458313 },
-        medium: { label: '$4.00k', size: 0.3568248232305542 },
-        small: { label: '$1.00k', size: 0.1784124116152771 },
+        large: { label: '$10K', size: 10000 },
+        medium: { label: '$5K', size: 5000 },
+        small: { label: '$1K', size: 1000 },
+        maxSize: 10000
       })
     })
 
-    it('computes top, middle, and bottom legend bubble sizes for zMax=1000000', function () {
+    it('computes large, medium and small legend bubble sizes for zMax=1000000', function () {
       const legendBubbles = LegendUtils.getLegendBubbles(1000000, '', '%')
 
       expect(legendBubbles).toEqual({
-        large: { label: '900k%', size: 0.5352372348458313 },
-        medium: { label: '400k%', size: 0.3568248232305542 },
-        small: { label: '100k%', size: 0.1784124116152771 },
+        large: { label: '1M%', size: 1000000 },
+        medium: { label: '.5M%', size: 500000 },
+        small: { label: '.1M%', size: 100000 },
+        maxSize: 1000000
       })
     })
   })
