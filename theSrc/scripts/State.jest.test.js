891349435b9d02ea40509f201ad8c23b532269a5
Justin Yap <JustinCCYap@users.noreply.github.com>
2022-01-21 16:42:20 +1100

    Add test

--- theSrc/scripts/State.jest.test.js
+++ theSrc/scripts/State.jest.test.js
@@ -2,8 +2,6 @@ const State = require('./State.js')
 
 describe('VIS-576', function () {
   test('X, Y, label properties preserved after state reset', function () {
-    // This is the state object associated with a widget, not to be confused with the instance "state" of the class State
-    const stateObj = {}
     const X = [1, 2, 3]
     const Y = [4, 5, 6]
     const label = ['A', 'B', 'C']
@@ -13,16 +11,47 @@ describe('VIS-576', function () {
       x: 200,
       y: 100,
     }
-    let isReset = false
+    let isCallbackFromReset = false
     const stateChangedCallback = obj => {
-      if (isReset) {
+      if (isCallbackFromReset) {
         expect(obj).toEqual({ X: X, Y: Y, label: label })
       }
     }
 
-    const state = new State(stateObj, stateChangedCallback, X, Y, label)
+    const state = new State({}, stateChangedCallback, X, Y, label)
     state.pushUserPositionedLabel(2, 600, 400, viewBox)
-    isReset = true
+    isCallbackFromReset = true
     state.resetState()
+  })
+})
+
+describe('VIS-572', function () {
+  test('X, Y, label properties preserved after resize', function () {
+    const X = [1, 2, 3]
+    const Y = [4, 5, 6]
+    const label = ['A', 'B', 'C']
+    const viewBox = {
+      width: 800,
+      height: 600,
+      x: 200,
+      y: 100,
+    }
+    const newViewBox = {
+      width: 1000,
+      height: 800,
+      x: 200,
+      y: 100,
+    }
+    let isCallbackFromReset = false
+    const stateChangedCallback = obj => {
+      if (isCallbackFromReset) {
+        expect(obj).toEqual({ X: X, Y: Y, label: label, userPositionedLabs: [], vb: newViewBox })
+      }
+    }
+
+    const state = new State({}, stateChangedCallback, X, Y, label)
+    state.pushUserPositionedLabel(2, 600, 400, viewBox)
+    isCallbackFromReset = true
+    state.resetStateOnResize(newViewBox)
   })
 })
